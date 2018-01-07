3d00a2eb3872092d54e197e57caf65a240e8efe5
Garrett Rose <garrett.rose03@gmail.com>
2018-01-07 14:16:40 -0500

    full functionality with conditions

--- handle.php
+++ handle.php
@@ -1,12 +1,30 @@
 <?php
   $items = Array('Rock','Paper','Scissors');
   $randomChoice = $items[array_rand($items)];
-  $choice = $_POST['choice'];
+  $userChoice = $_POST['userChoice'];
 
-  echo "<em>You Chose: </em>$choice</br>";
+  echo "<em>You Chose: </em>$userChoice</br>";
   echo "<em>The Computer Chose: </em>$randomChoice</br>";
-  if ($choice == $randomChoice) {
-    echo "Its a draw!";
-  } elseif ($choice == 'Paper' && $randomChoice == 'Scissors'){
-     echo "You loose to the pc!";
-}
+  //Draw condition
+  if ($userChoice == $randomChoice) {
+    echo "Its a draw!</br></br>RE-DIRECTING in 3 seconds";
+    header( "refresh:3;url=index.html" );
+  } elseif ($userChoice == 'Paper' && $randomChoice == 'Scissors'){
+     echo "You lose to the pc!";
+     header( "refresh:3;url=index.html" );
+   } elseif ($userChoice == 'Scissors' && $randomChoice == 'Paper'){
+      echo "You Win!";
+      header( "refresh:3;url=index.html" );
+    } elseif ($userChoice == 'Rock' && $randomChoice == 'Paper'){
+       echo "You lose to the pc!";
+       header( "refresh:3;url=index.html" );
+     } elseif ($userChoice == 'Paper' && $randomChoice == 'Rock'){
+        echo "You Win!";
+        header( "refresh:3;url=index.html" );
+      } elseif ($userChoice == 'Rock' && $randomChoice == 'Scissors'){
+         echo "You Win!";
+         header( "refresh:3;url=index.html" );
+       } else {
+          echo "You lose to the pc!";
+          header( "refresh:3;url=index.html" );
+        }
